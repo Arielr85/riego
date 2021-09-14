0de66f59d853ed9c64748e1c9295c384e9ed3d4c
Ariel Riaño <arielr85@gmail.com>
2021-09-14 14:04:43 -0500

    inicio de repositorio

---
+++ data/PuntosAjuste_4.js
@@ -0,0 +1,1 @@
+var json_PuntosAjuste_4 = {"type":"FeatureCollection","name":"PuntosAjuste_4","crs":{"type":"name","properties":{"name":"urn:ogc:def:crs:OGC:1.3:CRS84"}},"features":[{"type":"Feature","properties":{"id":null},"geometry":{"type":"Point","coordinates":[-72.856553218581965,4.530053788605199]}}]}
